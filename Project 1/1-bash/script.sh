0a323b2fa00f0f956e577f7bb0d623f24485e913
stamatiaktz <ktzstamatia@gmail.com>
2024-09-13 23:25:31 +0300

    final
    For Windows only, syntax error when used in Unix

--- Project 1/1-bash/script.sh
+++ Project 1/1-bash/script.sh
@@ -17,36 +17,43 @@ do
   echo "[5] Αποθήκευση αρχείου"
   echo "[6] Έξοδος"
   echo " "
-  read choice
+  read -r choice
 
   if [ "$choice" == "1" ]; then
     echo "Επιλέχθηκε η λειτουργία [1] Επιλογή αρχείου επιχειρήσεων"
-    read file 
+    echo "Παρακαλώ επιλέξτε αρχείο"
+    read -r file 
 
     if [ -z "$file" ]; then 
-      echo "Δεν βρέθηκε αρχείο \"$file\". Το αρχείο με όνομα Businesses.csv επιλέχθηκε αυτόματα."
+      echo "Δεν βρέθηκε αρχείο $file. Το αρχείο με όνομα Businesses.csv επιλέχθηκε αυτόματα."
       file="Businesses.csv"
     else
-      echo "Το αρχείο με όνομα \"$file\" επιλέχθηκε."
+      echo "Το αρχείο με όνομα $file επιλέχθηκε."
     fi
 
   elif [ "$choice" == "2" ]; then
     echo "Επιλέχθηκε η λειτουργία [2] Προβολή στοιχείου επιχείρησης"
+    echo "Παρακαλώ επιλέξτε αρχείο"
+    read -r file 
+
     if [ -z "$file" ]; then 
-      echo "Δεν βρέθηκε αρχείο \"$file\". Το αρχείο με όνομα Businesses.csv επιλέχθηκε αυτόματα."
-      file="Businesses.csv"      
+      echo "Δεν βρέθηκε αρχείο $file. Το αρχείο με όνομα Businesses.csv επιλέχθηκε αυτόματα."
+      file="Businesses.csv"
+    else
+      echo "Το αρχείο με όνομα $file επιλέχθηκε."
     fi
+    
     echo "Εισαγωγή κωδικού επιχείρησης"
-    read code
+    read -r code
 
     search=$(awk -F',' -v pass="$code" '$1 == pass {print}' "$file")
 
     if [ -z "$search" ]; then 
-      echo "Δεν υπάρχει επιχείρηση με κωδικό '$code'."
+      echo "Δεν υπάρχει επιχείρηση με κωδικό $code ."
     else     
-      IFS=',' read code name address city post_id longitude latitude <<< "$search"
+      IFS=',' read -r code name address city post_id longitude latitude <<< "$search"
 
-      echo "Προβολή των στοιχείων της επιχείρησης με κωδικό '$code'."
+      echo "Προβολή των στοιχείων της επιχείρησης με κωδικό $code."
       echo "κωδικός: $code"
       echo "όνομα: $name"
       echo "οδός: $address"
@@ -58,17 +65,23 @@ do
 
   elif [ "$choice" == "3" ]; then
     echo "Επιλέχθηκε η λειτουργία [3] Αλλαγή στοιχείου επιχείρησης"
+    echo "Παρακαλώ επιλέξτε αρχείο"
+    read -r file 
+
     if [ -z "$file" ]; then 
-      echo "Δεν βρέθηκε αρχείο \"$file\". Το αρχείο με όνομα Businesses.csv επιλέχθηκε αυτόματα."
-      file="Businesses.csv"      
+      echo "Δεν βρέθηκε αρχείο $file. Το αρχείο με όνομα Businesses.csv επιλέχθηκε αυτόματα."
+      file="Businesses.csv"
+    else
+      echo "Το αρχείο με όνομα $file επιλέχθηκε."
     fi
+
     echo "Δώστε τον κωδικό επιχείρησης"
-    read code
+    read -r code
 
     search=$(awk -F',' -v pass="$code" '$1 == pass {print}' "$file")
 
     if [ -z "$search" ]; then 
-      echo "Δεν υπάρχει επιχείρηση με κωδικό '$code'."
+      echo "Δεν υπάρχει επιχείρηση με κωδικό $code."
     else 
       echo "Επιλογή στοιχείου προς αλλαγή:"
       echo "1. κωδικός"
@@ -79,51 +92,53 @@ do
       echo "6. γεωγραφικό μήκος"
       echo "7. γεωγραφικό πλάτος"
 
-      read option
-
+      read -r option
+     
       temp_file=$(mktemp)
+      trap 'rm -f "$temp_file"' EXIT
       cp "$file" "$temp_file"  
+      
       case $option in
       1)
         echo "Καταχώρηση νέου κωδικού:"
-        read n_code
-        awk -v pass="$code" -v n_code="$n_code" 'BEGIN{FS=OFS=","} $1 == pass {$1=n_code}1' "$temp_file" > temp_file.tmp && mv temp_file.tmp "$temp_file"
+        read -r n_code
+        awk -v pass="$code" -v n_code="$n_code" 'BEGIN{FS=OFS=","} $1 == pass {$1=n_code}{print}'"$temp_file" > "$file.tmp" && mv "$file.tmp" "$file"
       ;;
   
       2)
         echo "Καταχώρηση νέου ονόματος:"
-        read n_name
-        awk -v pass="$code" -v n_name="$n_name" 'BEGIN{FS=OFS=","} $1 == pass {$2=n_name}1' "$temp_file" > temp_file.tmp && mv temp_file.tmp "$temp_file"
+        read -r n_name
+        awk -v pass="$code" -v n_name="$n_name" 'BEGIN{FS=OFS=","} $1 == pass {$2=n_name}{print}'"$temp_file" > "$file.tmp" && mv "$file.tmp" "$file"
       ;;
   
       3)
         echo "Καταχώρηση νέας οδού:"
-        read n_address
-        awk -v pass="$code" -v n_address="$n_address" 'BEGIN{FS=OFS=","} $1 == pass {$3=n_address}1' "$temp_file" > temp_file.tmp && mv temp_file.tmp "$temp_file"
+        read -r n_address
+        awk -v pass="$code" -v n_address="$n_address" 'BEGIN{FS=OFS=","} $1 == pass {$3=n_address}{print}'"$temp_file" > "$file.tmp" && mv "$file.tmp" "$file"
       ;;
   
       4)
         echo "Καταχώρηση νέας πόλης:"
-        read n_city
-        awk -v pass="$code" -v n_city="$n_city" 'BEGIN{FS=OFS=","} $1 == pass {$4=n_city}1' "$temp_file" > temp_file.tmp && mv temp_file.tmp "$temp_file"
+        read -r n_city
+        awk -v pass="$code" -v n_city="$n_city" 'BEGIN{FS=OFS=","} $1 == pass {$4=n_city}{print}'"$temp_file" > "$file.tmp" && mv "$file.tmp" "$file"
       ;;
   
       5)
         echo "Καταχώρηση νέου ταχυδρομικού κώδικα:"
-        read n_postalcode
-        awk -v pass="$code" -v n_postalcode="$n_postalcode" 'BEGIN{FS=OFS=","} $1 == pass {$5=n_postalcode}1' "$temp_file" > temp_file.tmp && mv temp_file.tmp "$temp_file"
+        read -r n_postalcode
+        awk -v pass="$code" -v n_postalcode="$n_postalcode" 'BEGIN{FS=OFS=","} $1 == pass {$5=n_postalcode}{print}'"$temp_file" > "$file.tmp" && mv "$file.tmp" "$file"
       ;;
   
       6)
         echo "Καταχώρηση νέου γεωγραφικού μήκους:"
-        read n_longitude
-        awk -v pass="$code" -v n_longitude="$n_longitude" 'BEGIN{FS=OFS=","} $1 == pass {$6=n_longitude}1' "$temp_file" > temp_file.tmp && mv temp_file.tmp "$temp_file"
+        read -r n_longitude
+        awk -v pass="$code" -v n_longitude="$n_longitude" 'BEGIN{FS=OFS=","} $1 == pass {$6=n_longitude}{print}'"$temp_file" > "$file.tmp" && mv "$file.tmp" "$file"
       ;;
   
       7)
         echo "Καταχώρηση νέου γεωγραφικού πλάτους:"
-        read n_latitude
-        awk -v pass="$code" -v n_latitude="$n_latitude" 'BEGIN{FS=OFS=","} $1 == pass {$7=n_latitude}1' "$temp_file" > temp_file.tmp && mv temp_file.tmp "$temp_file"
+        read -r n_latitude
+        awk -v pass="$code" -v n_latitude="$n_latitude" 'BEGIN{FS=OFS=","} $1 == pass {$7=n_latitude}{print}'"$temp_file" > "$file.tmp" && mv "$file.tmp" "$file"
       ;;
   
       *)
@@ -132,11 +147,11 @@ do
       esac
 
       # Εμφάνιση παλιών και νέων στοιχείων
-      IFS=',' read code name address city postal_code longitude latitude <<< "$search"
+      IFS=',' read -r code name address city postal_code longitude latitude <<< "$search"
 
       echo "Το στοιχείο που επιλέξατε άλλαξε."
       echo " "
-      echo "Προβολή παλιών και νέων στοιχείων της επιχείρησης με κωδικό '$code'."
+      echo "Προβολή παλιών και νέων στοιχείων της επιχείρησης με κωδικό $code."
       echo " "
       echo "Παλιά Στοιχεία:"
       echo "Κωδικός: $code"
@@ -217,44 +232,54 @@ do
         ;;
       esac
 
-      mv "$temp_file" "$file"
     fi
 
   elif [ "$choice" == "4" ]; then
     echo "Επιλέχθηκε η λειτουργία [4] Προβολή αρχείου"
+    echo "Για έξοδο από το αρχείο πατήστε q, για να συνεχίσετε πατήστε space"
+    echo "Παρακαλώ επιλέξτε αρχείο"
+    read -r file 
+    
     if [ -z "$file" ]; then 
-      echo "Δεν βρέθηκε αρχείο \"$file\". Το αρχείο με όνομα Businesses.csv επιλέχθηκε αυτόματα."
-      file="Businesses.csv"      
+      echo "Δεν βρέθηκε αρχείο $file. Το αρχείο με όνομα Businesses.csv επιλέχθηκε αυτόματα."
+      file="Businesses.csv"
+    else
+      echo "Το αρχείο με όνομα $file επιλέχθηκε."
     fi
+    
     less "$file"
 
   elif [ "$choice" == "5" ]; then
-    echo "Επιλέχθηκε η λειτουργία [5] Αποθήκευση αρχείου"
-    echo "Εισαγωγή path αρχείου πελατολογίου"
-    read file_path 
+    echo "Επιλέχθηκε η λειτουργία [5] Αποθήκευση αρχείου "
+    echo "Παρακαλώ επιλέξτε αρχείο"
+    read -r file 
 
     if [ -z "$file" ]; then 
-      echo "Δεν βρέθηκε αρχείο \"$file\". Το αρχείο με όνομα Businesses.csv επιλέχθηκε αυτόματα."
-      file="Businesses.csv"      
+      echo "Δεν δόθηκε αρχείο $file με πελατολόγιο."
+      exit 0
+    else
+      echo "Το αρχείο με όνομα $file επιλέχθηκε."
     fi
+
+    echo "Παρακαλώ εισάγετε το path που θα αποθηκευτεί το πελατολόγιο του αρχείου $file"
+    read -r file_path 
 
     if [ -z "$file_path" ]; then 
-      touch "Businesses.csv"
-      cp "$file" "Businesses.csv"
-      echo "Δεν δόθηκε path. Αυτόματη αποθήκευση στο αρχείο Business.csv"
+      echo "Δεν δόθηκε path. Η αποθήκευση πελατολογίου στο αρχείο Businesses.csv πραγματοποιήθηκε αυτόματα"
+      file_path="Businesses.csv"      
     else
-      touch "$file_path"
-      cp "$file" "$file_path"
-      echo "Αποθήκευση πελατολογίου στο αρχείο \"$file_path\"."
+      echo "Αποθήκευση πελατολογίου στο αρχείο "$file_path"."
     fi
-
+    touch "$file_path"
+    cp "$file" "$file_path"
   elif [ "$choice" == "6" ] ; then
     echo "Επιλέχθηκε [6] Έξοδος"
-    break 
+    exit 0
       
   else
     echo "Η λειτουργία που ζητήσατε δεν βρέθηκε"
-  
   fi
 
 done
+
+
